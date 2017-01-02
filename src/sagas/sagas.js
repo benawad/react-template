c6be32c9a7ef686a0868c5ee7faebef93e2626d5
ben awad <benawad97@gmail.com>
2017-01-01 23:38:07 -0600

    comment out sagas

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -2,17 +2,17 @@ import { takeEvery } from 'redux-saga';
 import { fork, call, put } from 'redux-saga/effects';
 
 
-function* call_(action) {
+//function* call_(action) {
   //const result = yield call(_);
   //yield put({type: '', result});
-}
+//}
 
-function* _Saga() {
-  yield* takeEvery('', call_);
-}
+//function* _Saga() {
+  //yield* takeEvery('', call_);
+//}
 
 export default function* root() {
   yield [
-    fork(_Saga),
+    //fork(_Saga),
   ]
 }
